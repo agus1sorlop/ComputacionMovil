571ec3f77352275e2355edd23baf1b4aa0994dbc
triniqi17 <trinidad.quintoi@um.es>
2023-04-30 17:22:56 +0200

    Añadido traducción ES/EN/FR + actuailzación frontend

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +17,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openLocationActivity(View v){
-        Intent intent = new Intent(this,LocationActivity.class);
+    public void openLocationActivity(View v) {
+        Intent intent = new Intent(this, LocationActivity.class);
         startActivity(intent);
     }
 
-    public void openTelActivity(View v){
-        Intent intent = new Intent(this,TelephonyActivity.class);
+    public void openTelActivity(View v) {
+        Intent intent = new Intent(this, TelephonyActivity.class);
         startActivity(intent);
     }
 
-    public void openMapActivity(View v){
-        Intent intent = new Intent(this,MapsActivity.class);
+    public void openMapActivity(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
 
+    public void cambiarIdiomaIngles(View view) {
+        Locale locale = new Locale("en");
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+
+
+    }
+
+    public void cambiarIdiomaFrances(View view) {
+        Locale locale = new Locale("fr");
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+
+    }
+
+    public void cambiarIdiomaEspanol(View view) {
+        Locale locale = new Locale("es");
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+
+
+    }
 }
